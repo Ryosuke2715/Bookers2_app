5db785c7e08dd6919cfc2f68d46961f18e26c615
RyosukeSuzuki <138420782+Ryosuke2715@users.noreply.github.com>
2023-07-20 10:33:32 +0000

    [UpDate] 課題３更新

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'users/show'
+  get 'users/edit'
+  get 'books/show'
+  get 'books/edit'
+  root "homes#top"
+  get "/homes/about" => "homes#about", as: "about"
+  devise_for :users
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
